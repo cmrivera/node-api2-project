26bed256dd99af9baf08eacc89bdd513a6524c2c
cmrivera <cmrivera0004@student.stcc.edu>
2020-08-11 20:08:56 -0400

    MVP met

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const postRouter = require("./users/post-router");
 
 //create express server on port 4000
 const server = express();
-const port = 4000;
 
 //make server use express.json middleware
 server.use(express.json());
 server.use(welcomeRouter);
 
 //use post-router endpoints
-server.use("/api/posts", postRouter);
+server.use(postRouter);
 
-server.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
+server.get("/", (req, res) => {
+  res.send("Welcome to our blog server");
 });
+
+module.exports = server;
--- users/post-router.js
+++ users/post-router.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const Posts = require("./data/db");
-const { post } = require("./welcome-router");
+const Posts = require("../data/db");
 
 const router = express.Router();
 
 //get request to find posts then display or respond with error
-router.get("/posts", (req, res) => {
+router.get("/posts/", (req, res) => {
   Posts.find(req.query)
     .then((posts) => {
       res.status(200).json(posts);
@@ -35,59 +34,41 @@ router.get("/posts/:id", (req, res) => {
 //post request to post a comment. require body from user,
 // if no body res with need body
 //if other error res with 500 for error posting data
-router.post("/", (req, res) => {
-  const post = req.body;
-
-  if (!post.title || !post.contents) {
-    res.status(400).json({ errormessage: "Missing user name or email " });
-  } else {
-    try {
-      Posts.insert(post).then((post) => {
-        res.status(201).json(post);
+router.post("/posts", (req, res) => {
+  Posts.insert(req.body)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "There was an error while saving the post to the database",
       });
-    } catch {
-      res
-        .status(500)
-        .json({ error: "there was an error inserting post to database" });
-    }
-  }
+    });
 });
 
 //create request for post to reply to post with a comment.
 //make id req, and body req, if not given res with error for body req
 //if issue finding post id res with post does not exist, if post found but another err res with 500 for other err inserting post
 
-router.post("/:id/comments", (req, res) => {
-  const id = req.params.id;
-  const comment = req.body;
-
-  comment.post_id = Number(id);
-
-  if (!comment.text) {
-    res.status(400).json({ errMessage: "please input text for comment" });
-  } else {
-    Posts.findPostComments(req.params.id)
-      .then((comments) => {
-        if (comments) {
-          Posts.insertComment(comment);
-          res.status(201).json(comment);
-        } else {
-          res.status(404).json({ message: "this post does not exist" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: "there was an error while inserting comment to database",
-        });
+router.post("/posts/:id/comments", (req, res) => {
+  const commentInfo = { ...req.body, post_id: req.params.id };
+  Posts.insertComment(commentInfo)
+    .then((comment) => {
+      res.status(201).json(comment);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "There was an error while saving the comment to the database",
+        err,
       });
-  }
+    });
 });
 
 //delete request, find post then req id to remove post
 //res with 200 if post cant be removed, respond with 404 if post can not be found
 
-router.delete("/:id", (req, res) => {
+router.delete("/posts/:id", (req, res) => {
   Posts.findById(req.params.id)
     .then((post) => {
       Posts.remove(req.params.id)
